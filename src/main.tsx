04008db548dd93b9dc69195075b937c02f08d040
Nenad Krstic <krsticnenad.dev@gmail.com>
2025-12-17 10:56:36 +0100

    chore: add prime react provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import "../node_modules/primeflex/primeflex.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { PrimeReactProvider } from "primereact/api";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </StrictMode>
 );
